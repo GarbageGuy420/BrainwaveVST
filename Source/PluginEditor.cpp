25f6dc8c3db152b35da2caa1e2af1603f6cf6feb
GarbageGuy420 <158623079+GarbageGuy420@users.noreply.github.com>
2025-08-13 18:15:55 -0400

    Implement basic JUCE plugin skeleton

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -1,1 +1,22 @@
-// Editor cpp content here
+#include "PluginEditor.h"
+
+BrainwaveVSTAudioProcessorEditor::BrainwaveVSTAudioProcessorEditor (BrainwaveVSTAudioProcessor& p)
+    : AudioProcessorEditor (&p), audioProcessor (p)
+{
+    setSize (400, 300);
+}
+
+BrainwaveVSTAudioProcessorEditor::~BrainwaveVSTAudioProcessorEditor() = default;
+
+void BrainwaveVSTAudioProcessorEditor::paint (juce::Graphics& g)
+{
+    g.fillAll (juce::Colours::black);
+    g.setColour (juce::Colours::white);
+    g.setFont (15.0f);
+    g.drawFittedText ("BrainwaveVST", getLocalBounds(), juce::Justification::centred, 1);
+}
+
+void BrainwaveVSTAudioProcessorEditor::resized()
+{
+}
+
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -1,1 +1,20 @@
-// Editor header content here
+#pragma once
+
+#include <JuceHeader.h>
+#include "PluginProcessor.h"
+
+class BrainwaveVSTAudioProcessorEditor : public juce::AudioProcessorEditor
+{
+public:
+    explicit BrainwaveVSTAudioProcessorEditor (BrainwaveVSTAudioProcessor&);
+    ~BrainwaveVSTAudioProcessorEditor() override;
+
+    void paint (juce::Graphics&) override;
+    void resized() override;
+
+private:
+    BrainwaveVSTAudioProcessor& audioProcessor;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (BrainwaveVSTAudioProcessorEditor)
+};
+
